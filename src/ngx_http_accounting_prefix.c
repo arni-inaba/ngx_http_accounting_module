924ca2a915f275dc8ed90ba23b67d261a77bf31a
arnik <arnik@temposoftware.com>
2016-07-18 13:47:42 +0000

    API specific prefix extraction

--- src/ngx_http_accounting_prefix.c
+++ src/ngx_http_accounting_prefix.c
@@ -1,31 +1,67 @@
 #include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "ngx_http_accounting_prefix.h"
 
+u_int find_end_of_path_part(u_char *start, ngx_http_request_t *r);
+
 ngx_str_t extract_routing_prefix(ngx_http_request_t *r)
 {
-
     u_char *start = r->uri.data;
 
     // We consider any request that either has an empty uri or does not start with a '/' to be malformed
-    if (start == NULL || *start != '/') {
+    if (start == NULL || *start != '/')
+    {
         return (ngx_str_t) {17, (u_char *)"malformed-request"};
     }
+    // We have established that the URI starts with a slash
     ++start;
-    u_char *cmp = start;
-    u_int len = 0;
-    // We allow alphanumeric characters, dashes and underscores in accounting_id. We will use anything 
-    // between the first slash and the next forbidden character(usually slash or whitespace) as the accounting_id
-    while (len < r->uri.len-1 && (isalnum(*cmp) || *cmp == '_' || *cmp == '-'))
+
+    // We need to consider 4 cases before we identify the namespace: root, 'rest', 'addons', 'private'
+    char *schemes[] = {"rest", "addons", "private"};
+    u_int num_schemes = 3;
+
+    u_int len = find_end_of_path_part(start, r);
+
+    if (len == 0)
     {
-        len++;
-        cmp++;
-    }
-    if (len == 0) {
         return (ngx_str_t) {7, (u_char *)"default"};
     }
+
+    ngx_str_t firstpart = (ngx_str_t) {len, start};
+    u_int i;
+    for (i = 0; i < num_schemes; ++i)
+    {
+        if (strncmp(schemes[i], (char *)firstpart.data, len) == 0)
+        {
+            // we've found a path schema, need to find the namespace
+            start += len;
+            if ( *start == '/')
+            {
+                ++start;
+                return (ngx_str_t) {find_end_of_path_part(start, r), start};
+            }
+        }
+    }
+
+
     // We return the desired length of the accounting_id along with a pointer to the first character in the
     // accounting_id, i.e. the char after the first '/'.
     return (ngx_str_t) {len, start};
+}
+
+u_int find_end_of_path_part(u_char *start, ngx_http_request_t *request)
+{
+    // We allow alphanumeric characters, dashes and underscores in path parts. We will use anything
+    // between the first slash and the next forbidden character(usually slash or whitespace) as the accounting_id
+    u_char *cmp = start;
+    u_int idx = 0;
+    while (  idx < request->uri.len-1 // uri.len includes the preceding slash which we want to ignore
+             && (isalnum(*cmp) || *cmp == '_' || *cmp == '-'))
+    {
+        ++idx;
+        ++cmp;
+    }
+    return idx;
 }
--- tests/test_accounting_id.c
+++ tests/test_accounting_id.c
@@ -75,7 +75,43 @@ void test_extract_prefix_from_request_returns_malformed_request_if_prefix_does_n
     assert(strcmp(expected_result, result.data) == 0);
 }
 
-ngx_str_t create_request_and_get_prefix(char uri[], int len) 
+void test_extract_namespace_from_request_rest_schema(void)
+{
+    char test_location[] = "/rest/namespace";
+    char expected_result[] =  "namespace";
+    ngx_str_t result = create_request_and_get_prefix(test_location, sizeof(test_location));
+
+    assert(strcmp(expected_result, result.data) == 0);
+}
+
+void test_extract_prefix_from_request_addons_schema(void)
+{
+    char test_location[] = "/addons/namespace";
+    char expected_result[] =  "namespace";
+    ngx_str_t result = create_request_and_get_prefix(test_location, sizeof(test_location));
+
+    assert(strcmp(expected_result, result.data) == 0);
+}
+
+void test_extract_prefix_from_request_private_schema(void)
+{
+    char test_location[] = "/private/namespace";
+    char expected_result[] =  "namespace";
+    ngx_str_t result = create_request_and_get_prefix(test_location, sizeof(test_location));
+
+    assert(strcmp(expected_result, result.data) == 0);
+}
+
+void test_extract_prefix_from_request_rest_schema_without_namespace_returns_empty(void)
+{
+    char test_location[] = "/rest";
+    char expected_result[] =  "";
+    ngx_str_t result = create_request_and_get_prefix(test_location, sizeof(test_location));
+
+    assert(strcmp(expected_result, result.data) == 0);
+}
+
+ngx_str_t create_request_and_get_prefix(char uri[], int len)
 {
     ngx_str_t result;
     ngx_http_request_t request;
@@ -86,7 +122,7 @@ ngx_str_t create_request_and_get_prefix(char uri[], int len)
 }
 
 
-int main() 
+int main()
 {
     test_extract_prefix_from_request_with_only_prefix();
     test_extract_prefix_from_request_terminates_on_slash();
@@ -95,6 +131,10 @@ int main()
     test_extract_prefix_from_request_returns_malformed_request_on_empty();
     test_extract_prefix_from_request_returns_malformed_request_on_null();
     test_extract_prefix_from_request_returns_malformed_request_if_prefix_does_not_start_with_slash();
+    test_extract_namespace_from_request_rest_schema();
+    test_extract_prefix_from_request_addons_schema();
+    test_extract_prefix_from_request_private_schema();
+    test_extract_prefix_from_request_rest_schema_without_namespace_returns_empty();
     printf("Tests passed!\n");
     return 0;
 }
